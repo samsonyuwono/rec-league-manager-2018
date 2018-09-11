457c8b984b846950c74ec635ef159904ad9b1598
github username <samson.yuwono@gmail.com>
2018-09-11 12:24:44 -0400

    added component for login

--- src/App.js
+++ src/App.js
@@ -1,18 +1,76 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      teams: []
+    };
+  }
+
+  componentDidMount() {
+    axios.defaults.headers.common["Authorization"] = localStorage.getItem(
+      "jwtToken"
+    );
+    axios
+      .get("/api/team")
+      .then(res => {
+        this.setState({ teams: res.data });
+        console.log(this.state.teams);
+      })
+      .catch(error => {
+        if (error.response.status === 401) {
+          this.props.history.push("/login");
+        }
+      });
+  }
+
+  logout = () => {
+    localStorage.removeItem("jwtToken");
+    window.location.reload();
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div class="container">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">
+              Rec League &nbsp;
+              {localStorage.getItem("jwtToken") && (
+                <button class="btn btn-primary" onClick={this.logout}>
+                  Logout
+                </button>
+              )}
+            </h3>
+          </div>
+          <div class="panel-body">
+            <table class="table table-stripe">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Wins</th>
+                  <th>Losses</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.teams.map(team => (
+                  <tr>
+                    <td>
+                      <Link to={`/show/${team._id}`}>{team.logo_url}</Link>
+                    </td>
+                    <td>{team.name}</td>
+                    <td>{team.wins}</td>
+                    <td>{team.losses}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     );
   }
