434be3471c8890f0164025dcbe8a4bc1e0e713bf
github username <samson.yuwono@gmail.com>
2018-09-11 12:17:19 -0400

    installed and configured required modules for react

--- routes/team.js
+++ routes/team.js
@@ -3,20 +3,52 @@ var router = express.Router();
 var mongoose = require("mongoose");
 var Team = require("../models/Team.js");
 
+var passport = require("passport");
+require("../config/passport")(passport);
+
+getToken = function(headers) {
+  if (headers && headers.authorization) {
+    var parted = headers.authorization.split(" ");
+    if (parted.length === 2) {
+      return parted[1];
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+};
+
 /* GET ALL TEAMS */
-router.get("/", function(req, res, next) {
-  Team.find(function(err, products) {
-    if (err) return next(err);
-    res.json(products);
-  });
+router.get("/", passport.authenticate("jwt", { session: false }), function(
+  req,
+  res
+) {
+  var token = getToken(req.headers);
+  if (token) {
+    Team.find(function(err, teams) {
+      if (err) return next(err);
+      res.json(teams);
+    });
+  } else {
+    return res.status(403).send({ success: false, msg: "Unauthorized." });
+  }
 });
 
 /* SAVE TEAM */
-router.post("/", function(req, res, next) {
-  Team.create(req.body, function(err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
+router.post("/", passport.authenticate("jwt", { session: false }), function(
+  req,
+  res
+) {
+  var token = getToken(req.headers);
+  if (token) {
+    Team.create(req.body, function(err, post) {
+      if (err) return next(err);
+      res.json(post);
+    });
+  } else {
+    return res.status(403).send({ success: false, msg: "Unauthorized." });
+  }
 });
 
 module.exports = router;
