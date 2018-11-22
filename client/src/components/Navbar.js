8300ca091c9ee833486ecc2d0b63254d349deb67
github username <samson.yuwono@gmail.com>
2018-11-22 11:33:35 -0500

    toggle list for mobile navbar working

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,88 +1,104 @@
-import React from "react";
+import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import "../assets/Navbar.scss";
 
-const Navbar = () => (
-  <div className="nav-wrapper">
-    <div className="toggle-menu">
-      <div className="bars" />
-      <div className="bars" />
-      <div className="bars" />
-    </div>
-    <ul className="nav-list">
-      <li>
-        <NavLink
-          to="/"
-          exact
-          activeStyle={{
-            background: "#D1D0CE"
-          }}
-        >
-          Home
-        </NavLink>
-      </li>
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showMenu: false };
+  }
+  toggle = event => {
+    this.setState({
+      showMenu: !this.state.showMenu
+    });
+  };
+  render() {
+    console.log(this.state.showMenu);
+    return (
+      <div
+        className={this.state.showMenu ? "toggle-list-wrapper" : "nav-wrapper"}
+      >
+        <div onClick={this.toggle} className="toggle-menu">
+          <div className="bars" />
+          <div className="bars" />
+          <div className="bars" />
+        </div>
+        <ul className={this.state.showMenu ? "toggle-list" : "nav-list"}>
+          <li>
+            <NavLink
+              to="/"
+              exact
+              activeStyle={{
+                background: "#D1D0CE"
+              }}
+            >
+              Home
+            </NavLink>
+          </li>
 
-      <li>
-        <NavLink
-          to="/teams"
-          exact
-          activeStyle={{
-            background: "#D1D0CE"
-          }}
-        >
-          Teams
-        </NavLink>
-      </li>
+          <li>
+            <NavLink
+              to="/teams"
+              exact
+              activeStyle={{
+                background: "#D1D0CE"
+              }}
+            >
+              Teams
+            </NavLink>
+          </li>
 
-      <li>
-        <NavLink
-          to="/teams/new"
-          exact
-          activeStyle={{
-            background: "#D1D0CE"
-          }}
-        >
-          Add a team
-        </NavLink>
-      </li>
+          <li>
+            <NavLink
+              to="/teams/new"
+              exact
+              activeStyle={{
+                background: "#D1D0CE"
+              }}
+            >
+              Add a team
+            </NavLink>
+          </li>
 
-      <li>
-        <NavLink
-          to="/players"
-          exact
-          activeStyle={{
-            background: "#D1D0CE"
-          }}
-        >
-          Players
-        </NavLink>
-      </li>
+          <li>
+            <NavLink
+              to="/players"
+              exact
+              activeStyle={{
+                background: "#D1D0CE"
+              }}
+            >
+              Players
+            </NavLink>
+          </li>
 
-      <li>
-        <NavLink
-          to="/players/new"
-          exact
-          activeStyle={{
-            background: "#D1D0CE"
-          }}
-        >
-          Add a player
-        </NavLink>
-      </li>
+          <li>
+            <NavLink
+              to="/players/new"
+              exact
+              activeStyle={{
+                background: "#D1D0CE"
+              }}
+            >
+              Add a player
+            </NavLink>
+          </li>
 
-      <li>
-        <NavLink
-          to="/teams/standings"
-          exact
-          activeStyle={{
-            background: "#D1D0CE"
-          }}
-        >
-          Standings
-        </NavLink>
-      </li>
-    </ul>
-  </div>
-);
+          <li>
+            <NavLink
+              to="/teams/standings"
+              exact
+              activeStyle={{
+                background: "#D1D0CE"
+              }}
+            >
+              Standings
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default Navbar;
