74f25a02ac2ef0669926de9b564cae00e93c2171
github username <samson.yuwono@gmail.com>
2018-12-01 15:25:15 -0500

    corrected secrets filepath

--- models/Team.js
+++ models/Team.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 var Schema = mongoose.Schema,
-  mongoUrl = require("../secrets");
+  mongoUrl = require("../config/secrets");
 
 mongoose.connect(mongoUrl.secrets.dbUri);
 
